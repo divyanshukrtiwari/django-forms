800e630d9c77fc774bde51e0466195861b00e965
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-22 01:53:46 +0530

    completed forms

--- pizzeria/pizza/views.py
+++ pizzeria/pizza/views.py
@@ -56,5 +56,7 @@ def edit_order(request, pk):
         if filled_form.is_valid():
             filled_form.save()
             form = filled_form
+            note = 'Order has been updated!'
+            return render(request, 'pizza/edit_order.html', {'pizzaform':form, 'pizza':pizza, 'note':note})
     return render(request, 'pizza/edit_order.html', {'pizzaform':form, 'pizza':pizza})
 
