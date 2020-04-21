b70e7ed14bdca879e769d30c43662daed13600c4
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-21 22:28:45 +0530

    added pizzas function

--- pizzeria/pizza/views.py
+++ pizzeria/pizza/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.forms import formset_factory
 
 from .forms import PizzaForm, MultiplePizzaForm
+
 
 # Create your views here.
 
@@ -19,4 +21,24 @@ def order(request):
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {"pizzaform": form, 'multiple_form': multiple_form})
+
+def pizzas(request):
+    number_of_pizzas = 2
+    filled_multiple_pizza_form = MultiplePizzaForm(request.GET)
+    if filled_multiple_pizza_form.is_valid():
+        number_of_pizzas = filled_multiple_pizza_form.cleaned_data['number']
+
+    PizzaFormSet = formset_factory(PizzaForm, extra=number_of_pizzas)
+    formset = PizzaFormSet()
+
+    if request.method == 'POST':
+        filled_formset = PizzaFormSet(request.POST)
+        if filled_formset.is_valid():
+            for form in filled_formset:
+                print(form.cleaned_data['topping1'])
+            note = 'Pizzas have been ordered!'
+        else:
+            note = 'Something went wrong. Please try again'
+
+        return render(request, 'pizza/pizzas.html', {'note':note, 'formset':formset})            
 
