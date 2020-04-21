e1da2c38de7d3aec463ba649a6d2fd14e61f806b
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-21 22:14:36 +0530

    added multiple pizza form

--- pizzeria/pizza/views.py
+++ pizzeria/pizza/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import PizzaForm
+from .forms import PizzaForm, MultiplePizzaForm
 
 # Create your views here.
 
@@ -8,14 +8,15 @@ def home(request):
     return render(request, 'pizza/home.html')
 
 def order(request):
+    multiple_form = MultiplePizzaForm()
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
             note = 'Your order of %s %s and %s pizza has been placed!' %(filled_form.cleaned_data['size'], 
             filled_form.cleaned_data['topping1'], filled_form.cleaned_data['topping2'])
             new_form = PizzaForm()
-            return render(request, 'pizza/order.html', {"pizzaform": new_form, 'note':note})
+            return render(request, 'pizza/order.html', {"pizzaform": new_form, 'note':note, 'multiple_form':multiple_form})
     else:
         form = PizzaForm()
-        return render(request, 'pizza/order.html', {"pizzaform": form})
+        return render(request, 'pizza/order.html', {"pizzaform": form, 'multiple_form': multiple_form})
 
--- pizzeria/pizzeria/urls.py
+++ pizzeria/pizzeria/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('order', views.order, name='order'),
+    path('pizza', views.pizzas, name='pizzas'),
 ]
